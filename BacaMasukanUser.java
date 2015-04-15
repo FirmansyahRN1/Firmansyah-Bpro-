d9c35c8d94b810241d01db8c280229a876edb66f
FirmansyahRN1 <14410100070@stikom.edu>
2015-04-15 19:24:10 +0700

    Create BacaMasukanUser.java

---
+++ BacaMasukanUser.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class BacaMasukanUser{
+    public static void main (String []argc) {
+      Scanner masukan= new Scanner (System.in) ;
+      System.out.println("Masukan bil int) : ") ;
+      int bil=masukan . nextInt() ;
+      System.out.println("bil= " + bil) ;
+      System.out.println("Masukan sebuah kata : ") ;
+      String dtr=masukan.next () ;
+      System.out.println ("kata = " + str) ;
+    }
+}
