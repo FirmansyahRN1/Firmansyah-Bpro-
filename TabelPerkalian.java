e93f1b61faae8c28c9633ada8cfea59e9b539cbe
FirmansyahRN1 <14410100070@stikom.edu>
2015-04-15 19:29:35 +0700

    Create TabelPerkalian.java

---
+++ TabelPerkalian.java
@@ -0,0 +1,20 @@
+public class TabelPerkalian2
+{
+ public static void main ( String[] args)
+ {
+ 
+  int a,b,c;
+  
+  for(a=1;a<=10;a++)
+  {
+   for(b=1;b<=10;b++)
+   {
+    c=a*b;
+    System.out.print(a +"x"+ b +"="+ c);
+    System.out.print("\t");
+   }
+  System.out.print("\n");
+
+  }
+ }
+}
